6e5ed69efc946c17690b5d554d447b9d1b4c05d8
Tommy Yu <tommy@zensports.com>
2022-03-31 11:11:24 -0400

    add adroll pixel to zensportsia.com website

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,6 +37,44 @@ export default function IndexPage() {
     }
   }, []);
 
+  useEffect(() => {
+    const script = document.createElement('script');
+  
+    script.innerHTML = `
+      adroll_adv_id = "4PXTIIXXEJDMLMFN2GI5NJ";
+      adroll_pix_id = "TI747JOZU5G2XEUB3CL4S7";
+      adroll_version = "2.0";
+
+      (function(w, d, e, o, a) {
+          w.__adroll_loaded = true;
+          w.adroll = w.adroll || [];
+          w.adroll.f = [ 'setProperties', 'identify', 'track' ];
+          var roundtripUrl = "https://s.adroll.com/j/" + adroll_adv_id
+                  + "/roundtrip.js";
+          for (a = 0; a < w.adroll.f.length; a++) {
+              w.adroll[w.adroll.f[a]] = w.adroll[w.adroll.f[a]] || (function(n) {
+                  return function() {
+                      w.adroll.push([ n, arguments ])
+                  }
+              })(w.adroll.f[a])
+          }
+
+          e = d.createElement('script');
+          o = d.getElementsByTagName('script')[0];
+          e.async = 1;
+          e.src = roundtripUrl;
+          o.parentNode.insertBefore(e, o);
+      })(window, document);
+      adroll.track("pageView");
+    `;
+  
+    document.body.appendChild(script);
+  
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
+
   return (
     <>
       <header>
